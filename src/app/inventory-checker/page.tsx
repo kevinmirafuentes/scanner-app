2460b1c41b6aab80e08097f98290cce40e16382b
Kevin <kevin.mirafuentes@gmail.com>
2024-06-13 01:49:43 +0800

    Add check inventory layout

--- src/app/inventory-checker/page.tsx
+++ src/app/inventory-checker/page.tsx
@@ -1,10 +1,10 @@
-import { Container, Input, InputGroup, InputRightElement, Text, VStack, Select, HStack, Box, Card, CardBody } from "@chakra-ui/react";
+import { Container, Input, InputGroup, InputRightElement, Text, VStack, Select, HStack, Box, Card, CardBody, Checkbox, Flex } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarcode } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "@/components/Navbar"
 import FooterNav from "@/components/FooterNav";
 
-export default function PriceChecker() {
+export default function InventoryChecker() {
   return (
     <VStack>
       <Navbar>
@@ -25,17 +25,12 @@ export default function PriceChecker() {
               ></FontAwesomeIcon>
             </InputRightElement>
           </InputGroup>
-        </Container>
-        <Container>
-          <Select 
-            placeholder='Select option' 
-            backgroundColor='teal.300'
-            color='white'
-          >
-            <option value='option1' style={{ color: 'black' }}>Option 1</option>
-            <option value='option2' style={{ color: 'black' }}>Option 2</option>
-            <option value='option3' style={{ color: 'black' }}>Option 3</option>
-          </Select>
+        </Container>         
+        <Container >
+          <HStack justify='end'>
+            <Text>Convert to Order Unit</Text>
+            <Checkbox defaultChecked></Checkbox>
+          </HStack>
         </Container>
         <Container>
           <Card>
@@ -49,29 +44,17 @@ export default function PriceChecker() {
                   <Text fontSize='xs'>123123123213</Text>
                 </HStack>
                 <Text fontSize='xl'>Lucky Me Pancit Canton</Text>
-                <Text fontSize='3xl' fontWeight='bold'>Php 100.00</Text>
-                <Box>
-                  <Text fontSize='xl'>[ 3@9.00]</Text>
-                  <Text fontSize='xl'>[ 6@9.00]</Text>
-                </Box>
+                <VStack spacing='0'>
+                  <Text fontSize='3xl' fontWeight='bold'>100</Text>
+                  <Text fontSize='md' fontWeight='bold'>Total Inventory</Text>
+                </VStack>
               </VStack>
             </CardBody>
           </Card>
         </Container>
-  
-        {/* Admin only view */}
-        <Container>
-          <HStack justify='space-between'>
-            <Text>Supplier</Text>
-            <HStack borderWidth='1px' border-color='gray.200'>
-              <Text padding='10px 15px'>0064</Text>
-              <Text padding='10px 15px' borderLeft='1px' borderColor='gray.200'>W.I. Food Products</Text>
-            </HStack>
-          </HStack>
-        </Container>
       </VStack>
       
-      <FooterNav active='price-checker'></FooterNav>
+      <FooterNav active='inventory-checker'></FooterNav>
     </VStack>
   );
 }

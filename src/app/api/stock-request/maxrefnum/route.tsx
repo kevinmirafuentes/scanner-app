5da4db1bbeb68d9bc6c4b0afc7eddae4e5b7d1f7
Kevin <kevin.mirafuentes@gmail.com>
2024-07-17 13:40:13 +0800

    Update maxrefnum if nothing is found

--- src/app/api/stock-request/maxrefnum/route.tsx
+++ src/app/api/stock-request/maxrefnum/route.tsx
@@ -3,5 +3,6 @@ import { getNextReferenceNumber } from "@/repository/stockRequest";
 
 export async function GET() {
   let res = await getNextReferenceNumber();
+  res.maxrefnum = res.maxrefnum || 1; 
   return apiResponse(res, 200);
 }

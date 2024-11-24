8fbdf740c9e0aa06706e7976af20158aec3c4745
Kevin <kevin.mirafuentes@gmail.com>
2024-11-25 01:36:03 +0800

    Update stock adjustment out backend

--- src/app/api/stock-adjustment-out/route.tsx
+++ src/app/api/stock-adjustment-out/route.tsx
@@ -1,39 +1,43 @@
 import { getCurrentBranch, getSession } from "@/auth";
 import { apiResponse } from "@/lib/utils";
 import { getBarcodeById } from "@/repository/barcodes";
-import { getNextReferenceNumber, saveStockAdjustmentOut } from "@/repository/stockAdjustmentOut";
+import { getProductOrderCostHistory } from "@/repository/products";
+import { getLatestRefNumber, getNextReferenceNumber, saveStockAdjustmentOut } from "@/repository/stockAdjustmentOut";
 import { StockAdjustmentOut, StockAdjustmentOutItem } from "@/types/types";
 
 const initStockAdjustmentOut = async (data: StockAdjustmentOut) => {
-  let nextRefNum = await getNextReferenceNumber();
+  let {maxrefnum} = await getNextReferenceNumber();
   let { branch_id } = await getCurrentBranch();
   let { user_id } = await getSession();
+  let items = data.items ? await Promise.all(data.items?.map(async (i) => await initStockAdjustmentOutItem(i))) : [];
 
-  // @TODO: clarify if this is correct default value of items below
-  data.status = 'A';
-
-  data.branch_id = branch_id;
-  data.adj_no = nextRefNum.maxrefnum.toString();
-  data.trans_date = new Date(data.trans_date||'');  
-  data.user_id = user_id;
-
-  if (data?.items) {
-    for (let i = 0; i < data.items?.length; i++) {
-      data.items[i] = await initStockAdjustmentOutItem(data.items[i]);
-    }
-  }
-  
-  return data;
+  return {
+    branch_id:      branch_id, 
+    adj_no:         maxrefnum.toString(),
+    status:         'A', 
+    trans_date:     new Date(data?.trans_date||''), 
+    remarks:        data.remarks, 
+    total_qty:      items.reduce((a, i: StockAdjustmentOutItem) => a + (i?.qty||0), 0), 
+    total_net_amt:  0, 
+    user_id:        user_id, 
+    posted:         0, 
+    branch_ref_no:  await getLatestRefNumber(branch_id), 
+    items:          items,
+  };
 }
 
 const initStockAdjustmentOutItem  = async (data: StockAdjustmentOutItem) => {
-  let barcodeInfo = data.barcode_id ? await getBarcodeById(data.barcode_id) : null;
+  let {unit_id, product_id, content_qty}  = data.barcode_id ? await getBarcodeById(data.barcode_id) : null;
+  let productOrderCostHistory = await getProductOrderCostHistory(product_id);
+  let unitPrice = productOrderCostHistory?.order_pcs_gross_cost || 0;
   return {
-    // @TODO: clarify if this is correct default value of items below
-    ...data,
-    unit_id: barcodeInfo?.unit_id, 
-    unit_price: barcodeInfo?.unit_cost,
-  };
+    barcode_id: data.barcode_id,
+    qty:        data.qty,
+    unit_id:    unit_id, 
+    unit_price: unitPrice,
+    base_qty:   (data?.qty||0) * (content_qty),  
+    net_amt:    0, 
+  }
 }
 
 export async function POST(
